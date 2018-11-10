6113c0275012a2ef7390a16d1394cab392adc2a9
ferrum848 <alex84_84_84@mail.ru>
2018-11-10 20:25:29 +0300

    result selfie foto

--- imagecrawler/spiders/pexels_scraper.py
+++ imagecrawler/spiders/pexels_scraper.py
@@ -1,5 +1,5 @@
 import scrapy
-import re
+import re, os, time, shutil
 from scrapy.linkextractor import LinkExtractor
 from scrapy.selector import Selector
 from imagecrawler.items import ImagecrawlerItem
@@ -14,30 +14,34 @@ class PexelsScraper(scrapy.Spider):
     
     # Create a set that'll keep track of ids we've crawled
     crawled_ids = set()
+
     
     def start_requests(self):
-        url = "https://www.pexels.com/"
+        url = "https://www.pexels.com/search/selfie"
         yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
 	    body = Selector(text=response.body)
 	    images = body.css('img.image-section__image').extract()
 	    result = body.css('img.image-section__image ::attr(src)').extract_first()
-	    #if result:
-	    #	result = re.split(r'?',result)
+
 
 	    #yield {'img_url': result}
 	    yield ImagecrawlerItem(file_urls=[result])
-
+	    
+	    if len(os.listdir('/work/imagecrawler/output/full')) > 0:
+	    	for name in os.listdir('/work/imagecrawler/output/full'):
+	    		temp = name.find('?')
+	    		new_name = name[:temp]
+	    		shutil.copy('/work/imagecrawler/output/full/' + name, '/work/imagecrawler/output/result/' + new_name)
 
 
 	    # body.css().extract() returns a list which might be empty
-	    for image in images:
-	        img_url = PexelsScraper.src_extractor.findall(image)[0]
-	        tags = [tag.replace(',', '').lower() for tag in PexelsScraper.tags_extractor.findall(image)[0].split(' ')]       
-	        
-	        yield {'img_url': img_url}
-	        yield {'img_tags': tags}
+	    #for image in images:
+	        #img_url = PexelsScraper.src_extractor.findall(image)[0]
+	        #tags = [tag.replace(',', '').lower() for tag in PexelsScraper.tags_extractor.findall(image)[0].split(' ')]   
+	        #yield {'img_url': img_url}
+	        #yield {'img_tags': tags}
 
 
 
